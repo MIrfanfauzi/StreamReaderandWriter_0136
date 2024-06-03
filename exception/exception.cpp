00c589cf56b5e7a0a30bbe3e767bf26316d45b10
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-06-03 10:28:55 +0700

    membuat array 3 elemen

--- exception/exception.cpp
+++ exception/exception.cpp
@@ -7,6 +7,11 @@ using namespace std;
 
 int main()
 {
-    
+    cout << "Awal Program " << endl; //penanda 1:...
+    try {
+        array< int, 3> data = { 1, 2, 3 };
+        //pesan array interger 3 elemen
+        
+    }
 }
 
