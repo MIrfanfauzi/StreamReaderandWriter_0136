ded35d16564dd321bb681e31713f52dae6fd07e8
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-06-03 10:29:12 +0700

    memanggil array elemen ke 5

--- exception/exception.cpp
+++ exception/exception.cpp
@@ -11,7 +11,8 @@ int main()
     try {
         array< int, 3> data = { 1, 2, 3 };
         //pesan array interger 3 elemen
-        
+        cout << data.at(5) << endl;
+        //memanggil array elemen ke 5
     }
 }
 
