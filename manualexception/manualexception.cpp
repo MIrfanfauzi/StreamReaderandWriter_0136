274ede669c9646127bdc84c06572f4c20ca2f0ac
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-06-03 10:36:05 +0700

    melemparkan sebuah interger

---
+++ manualexception/manualexception.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan digunakan
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        //melemparkan sebuah interger
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    
+}
+
